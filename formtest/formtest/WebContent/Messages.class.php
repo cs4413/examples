2912c1639b47566eda6a2eca4297e45c5707408a
kayrobbins <robbins.kay@outlook.com>
2015-09-03 17:18:21 -0500

    Started refactoring to support internationalization

---
+++ formtest/formtest/WebContent/Messages.class.php
@@ -0,0 +1,22 @@
+<?php
+class Messages {
+	public static $errors = array();
+	
+	public static function setErrors($filename) {
+		$array = array();
+		foreach (file($filename) as $line) {
+			list($key, $value) = explode(' ', $line, 2) + array(NULL, NULL);
+			if ($value !== NULL)
+				$array[$key] = $value;
+		}
+		self::$errors = $array;
+	}
+	
+	public static function getError($errorName) {
+		if (isset(self::$errors, $errorName))
+			return self::$errors[$errorName];
+		else 
+			return "";
+	}
+	
+}
---
+++ formtest/formtest/WebContent/Messages_tests.php
@@ -0,0 +1,19 @@
+
+</head>
+<body>
+<h1>UserData tests</h1>
+
+<?php
+include_once("Messages.class.php");
+?>
+
+<h2>It should set errors from a file</h2>
+<?php 
+Messages::setErrors("errors_English.txt");
+
+echo "LAST_NAME_TOO_SHORT: " .Messages::getError("LAST_NAME_TOO_SHORT")."<br>";
+echo "EMAIL_INVALID: " .Messages::getError("EMAIL_INVALID")."<br>";
+echo "LAST_NAME_INVALID: " .Messages::getError("LAST_NAME_INVALID")."<br>";
+
+?>
+
