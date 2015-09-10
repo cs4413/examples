e5770d68f25f95b8d3885c3058857586ced83767
kayrobbins <robbins.kay@outlook.com>
2015-09-10 18:48:13 -0500

    Added testing for Spanish in Messages

--- mvctest/mvctest/WebContent/tests/Messages_tests.php
+++ mvctest/mvctest/WebContent/tests/Messages_tests.php
@@ -9,11 +9,12 @@ include_once("../models/Messages.class.php");
 
 <h2>It should set errors from a file</h2>
 <?php 
-Messages::setErrors("errors_English.txt");
+
+Messages::setErrors("../resources/errors_English.txt");
 
 echo "LAST_NAME_TOO_SHORT: " .Messages::getError("LAST_NAME_TOO_SHORT")."<br>";
 echo "EMAIL_INVALID: " .Messages::getError("EMAIL_INVALID")."<br>";
-echo "LAST_NAME_INVALID: " .Messages::getError("LAST_NAME_INVALID")."<br>";
+echo "LAST_NAME_INVALID: " .Messages::getError("LAST_NAME_HAS_INVALID_CHARS")."<br>";
 
 echo (empty(Messages::getError("LAST_NAME_TOO_SHORT")))?
       "Failed: it did not set LAST_NAME_TOO_SHORT from file":"";
@@ -25,7 +26,19 @@ Messages::reset();
 
 echo "LAST_NAME_TOO_SHORT: " .Messages::getError("LAST_NAME_TOO_SHORT")."<br>";
 echo "EMAIL_INVALID: " .Messages::getError("EMAIL_INVALID")."<br>";
-echo "LAST_NAME_HAS_INVALID_CHARS: " .Messages::getError("HAS_INVALID_CHARS")."<br>";
+echo "LAST_NAME_HAS_INVALID_CHARS: " .Messages::getError("LAST_NAME_HAS_INVALID_CHARS")."<br>";
 
 ?>
+
+<h2>It should allow change of locale</h2>
+<?php 
+Messages::$locale = 'Spanish';
+Messages::reset();
+
+echo "LAST_NAME_TOO_SHORT: " .Messages::getError("LAST_NAME_TOO_SHORT")."<br>";
+echo "EMAIL_INVALID: " .Messages::getError("EMAIL_INVALID")."<br>";
+echo "LAST_NAME_HAS_INVALID_CHARS: " .Messages::getError("LAST_NAME_HAS_INVALID_CHARS")."<br>";
+
+?>
+
 
