f15f4541ae24259b916f997e39558c27fcb9f96e
kayrobbins <robbins.kay@outlook.com>
2015-09-23 12:07:54 -0500

    Added a UserView test and Master call.

--- mvcform/mvcform/WebContent/views/UserView.class.php
+++ mvcform/mvcform/WebContent/views/UserView.class.php
@@ -1,8 +1,13 @@
-<?php
+<?php  
 class UserView {
-
-	public static function show() {
-		echo "I'm the User view";
+	public static function show($user) {
+		MasterView::showHeader("ClassBash User Page");
+		UserView::showDetails($user);
+		MasterView::showFooter("<h3>The footer goes here</h3>");
+	}
+	
+	public static function showDetails($user) {
+	   echo "I am the user";
 	}
 }
-?>
+?>	
