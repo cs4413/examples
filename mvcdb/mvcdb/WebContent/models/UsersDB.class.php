bde558786194ad00aa76d652c2954b423607d0e0
kayrobbins <robbins.kay@outlook.com>
2015-09-29 06:09:32 -0500

    Changed UsersDB:getAll to UsersDB:getAllUsers

--- mvcdb/mvcdb/WebContent/models/UsersDB.class.php
+++ mvcdb/mvcdb/WebContent/models/UsersDB.class.php
@@ -1,7 +1,28 @@
 <?php
 class UsersDB {
+	
+	public static function addUser($user) {
+		// Inserts the User object $user into the Users table and returns userId
+		$query = "INSERT INTO USERS (userName, password)
+		                      VALUES(:userName, :password)";
+		$returnId = 0;
+		try {
+			if (is_null($user) || $user->getErrorCount() > 0)
+				throw new PDOException("Invalid User object can't be inserted");
+			$db = Database::getDB ();
+			$statement = $db->prepare ($query);
+			$statement->bindValue(":userName", $user->getUserName());
+			$statement->bindValue(":password", $user->getPassword());	
+			$statement->execute ();
+			$statement->closeCursor();
+			$returnId = $db->lastInsertId("userId");
+		} catch ( PDOException $e ) { // Not permanent error handling
+			echo "<p>Error adding user to Users ".$e->getMessage()."</p>";
+		}
+		return $returnId;
+	}
 
-	public static function getAll() {
+	public static function getAllUsers() {
 	   $query = "SELECT * FROM USERS";
 	   $users = array();
 	   try {
@@ -15,6 +36,7 @@ class UsersDB {
 		}
 		return $users;
 	}
+	
 	
 	public static function getUsersArray($rowSets) {
 		$users = array();
--- mvcdb/mvcdb/WebContent/tests/UsersDB_tests.php
+++ mvcdb/mvcdb/WebContent/tests/UsersDB_tests.php
@@ -5,15 +5,29 @@ include("../models/User.class.php");
 include("../models/UsersDB.class.php");
 
 echo "<h1>Tests for UsersDB</h1>";
-
-echo "<h2>It should create get all users from a test database</h2>";
-
 $myDb = makeDB('mytest');
 
-$users = UsersDB::getAll();
+echo "<h2>It should create get all users from a test database</h2>";
+$users = UsersDB::getAllUsers();
 $userCount = count($users);
 echo "Number of users in db is: $userCount <br>";
-
 foreach ($users as $user) 
 	echo "$user <br>";
+	
+
+echo "<h2>It should allow a user to be added</h2>";
+echo "Number of users in db before added is: ". count(UsersDB::getAllUsers()) ."<br>";
+$validTest = array("userName" => "krobbins", "password" => "123");
+$user = new User($validTest);
+$userId = UsersDB::addUser($user);
+echo "Number of users in db after added is: ". count(UsersDB::getAllUsers()) ."<br>";
+echo "User ID of new user is: $userId<br>";
+
+
+echo "<h2>It should not add an invalid user</h2>";
+echo "Number of users in db before added is: ". count(UsersDB::getAllUsers()) ."<br>";
+$invalidUser = new User(array("userName" => "krobbins$"));
+$userId = UsersDB::addUser($invalidUser);
+echo "Number of users in db after added is: ". count(UsersDB::getAllUsers()) ."<br>";
+echo "User ID of new user is: $userId<br>";
 ?>
